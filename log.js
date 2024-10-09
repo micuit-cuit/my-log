af1978fca9fd31edc799714b7b4a9439466fa6a1
micuit-cuit <micuit-cuit>
2024-10-09 11:55:29 +0200

    Add mi-log configuration file and update test.html and test.js

--- log.js
+++ log.js
@@ -1,11 +1,19 @@
 //----------------------------
 //----creat by micuit-cuit----
 //----------------------------
+//dectecte l'environnement (nodejs ou navigateur)
+function isolateMiLog(){
+const isNode = typeof process !== 'undefined' && process.versions != null && process.versions.node != null;
+//dectecte si le terminal supporte les couleurs rgb ou ascii
 
+const darkColor = [
+  "blue"
+]
 class Log {
-  constructor(stack = []) {
+  constructor(stack = [color, text], textColor = true) {
     // stack est un tableau de string à afficher avant le message
-    this.stack = stack;
+    this.prefixe = stack;
+    this.textColor = textColor;
   }
 
   getStackInfo() {
@@ -14,8 +22,11 @@ class Log {
     return match ? { func: match[1], file: match[2].split('/').pop(), line: match[3] } : { func: '', file: 'unknown', line: '0' };
   }
   colorize(color) {
+    if (!isNode) {
+      return { fg: 'black', bg: color };
+    }
     const colors = {
-      reset: '\x1b[0m',
+      reset: '\x1b[0m\x1b[40m',
       black: { fg: '\x1b[30m', bg: '\x1b[40m' },
       red: { fg: '\x1b[31m', bg: '\x1b[41m' },
       green: { fg: '\x1b[32m', bg: '\x1b[42m' },
@@ -36,7 +47,7 @@ class Log {
     if (colors[color]) {
       return colors[color];
     }else{
-      console.error(`Color ${color} not found`);
+      console.error(`Color ${color} not found in your terminal use different color or install a terminal that support it`);
       return colors['red'];
     }
   }
@@ -48,67 +59,209 @@ class Log {
     switch (level) {
       case 'INFO':
         emoji = 'ℹ️';
-        color = this.colorize( 'brightBlue').fg+this.colorize('black').bg;
+        color = this.colorize('brightBlue').fg+this.colorize('black').bg+":dodgerblue";
         break;
       case 'WARN':
         emoji = '⚠️';
-        color = this.colorize( 'brightYellow').fg+this.colorize('black').bg;
+        color = this.colorize('brightYellow').fg+this.colorize('black').bg+":orange";
         break;
       case 'ERROR':
         emoji = '🚫';
-        color = this.colorize( 'brightRed').fg+this.colorize('black').bg;
+        color = this.colorize('brightRed').fg+this.colorize('black').bg+":red";
         break;
       case 'DEBUG':
         emoji = '🐛';
-        color = this.colorize( 'brightMagenta').fg+this.colorize('black').bg;
+        color = this.colorize('brightMagenta').fg+this.colorize('black').bg+":purple";
         break;
       default:
         emoji = '✅';
-        color = this.colorize( 'brightGreen').fg+this.colorize('black').bg;
+        color = this.colorize('brightGreen').fg+this.colorize('black').bg+":green";
+    }
+    if (!isNode) {
+      color = color.split(':')[1];
+    }else{
+      color = color.split(':')[0];
     }
 
     let style = [];
     let message = '';
-    let stack = [
+    let prefixes = [
       { style: 'circle', color: color, text: `${emoji} ${level}` },
-      { style: 'square', color: this.colorize('yellow').fg+this.colorize('black').bg, text: `${time}` },
+      { style: 'square', color: isNode?this.colorize('yellow').fg+this.colorize('black').bg:"yellow", text: `${time}` },
       { style: 'square', color: color, text: `${file}:${line}` },
-      ...this.stack.map((item) => {
-        return { style: item.style, color: this.colorize(item.color).fg+this.colorize('black').bg, text: item.text };
+      ...this.prefixe.map((item) => {
+        return { style:"circle", color: isNode?this.colorize(item.color).fg+this.colorize('black').bg:item.color, text: item.text };
       })
     ];
-
-    for (let i = 0; i < stack.length; i++) {
-      if (stack[i].style === 'circle') {
-        style.push(stack[i].color +'(' + stack[i].text + ')' + this.colorize('reset'));
-      } else {
-        style.push(stack[i].color +"["+ stack[i].text +"]"+ this.colorize('reset'));
+    if (isNode){ 
+      for (let i = 0; i < prefixes.length; i++) {
+        if (prefixes[i].style === 'circle') {
+          style.push(prefixes[i].color +'(' + prefixes[i].text + ')' + this.colorize('reset'));
+        } else {
+          style.push(prefixes[i].color +"["+ prefixes[i].text +"]"+ this.colorize('reset'));
+        }
       }
+      let args2 = [];
+      if (this.textColor) {
+        for (let i = 0; i < args.length; i++) {
+          let str = ""
+          if (typeof args[i] == "object") {
+              str = args[i]
+              args2.push(str);
+              continue;
+          }
+          str = color + args[i] + this.colorize('reset');
+          args2.push(str);
+        }
+      }else{args2 = [...args]}     
+      console.log(style.join(' '), ...args2, this.colorize('reset'));
+    }else{
+      for (let i = 0; i < prefixes.length; i++) {
+        if (!prefixes[i].style == "square") {
+          style.push(`background: ${prefixes[i].color}; color: ${darkColor.includes(prefixes[i].color) ? "white" : "black"}; font-weight: bold; border-radius: 5px;`);
+          style.push("["+prefixes[i].text+"]");
+        }else{
+          style.push(`background: ${prefixes[i].color};  color: ${darkColor.includes(prefixes[i].color) ? "white" : "black"}; font-weight: bold; border-radius: 3px;`);
+          style.push("("+prefixes[i].text+")");
+        }
+        style.push("");
+        if (typeof prefixes[i].text == "object") {
+          message += "%c%O%c ";
+        }else{
+          message += "%c%s%c ";
+        }
+      }
+      message += " ";
+      if (this.textColor) {
+        //ajout de la couleur du texte à la fin du tableau
+        for (let i = 0; i < args.length; i++) {
+
+          
+          if (typeof args[i] == "object") {
+            style.push(`color: ${color}; font-weight: bold;`);
+            message += "%c%o ";
+          }else{
+            style.push(`color: ${color}; font-weight: bold;`);
+            message += "%c%s ";
+          }
+          style.push(args[i]);
+        }
+      }
+      let args2 = [];
+      if (!this.textColor)args2 = [...args]
+
+      let messages = [message,...style, ...args2];
+      console.log(
+        ...messages
+      );
     }
-    console.log(style.join(' '), ...args);
+  }
+  createPopup(level, message) {
+    const randId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + "-MI-LOG";
+    let emoji, backgroundColor;
+    switch (level) {
+      case 'INFO':
+        emoji = 'ℹ️'; backgroundColor = 'dodgerblue'; break;
+      case 'WARN':
+        emoji = '⚠️'; backgroundColor = 'orange'; break;
+      case 'ERROR':
+        emoji = '🚫'; backgroundColor = 'red'; break;
+      case 'DEBUG':
+        emoji = '🐛'; backgroundColor = 'purple'; break;
+      case 'SUCCESS':
+        emoji = '✅'; backgroundColor = 'green'; break;
+      default:
+        emoji = '✅'; backgroundColor = 'green';
+    }
+
+    let popupContainer = document.getElementById('popup-container-MI-LOG');
+    if (!popupContainer) {
+      const container = document.createElement('div');
+      container.id = 'popup-container-MI-LOG';
+      container.style.cssText = `
+        position: fixed;
+        top: 0;
+        right: 0;
+        display: flex;
+        justify-content: flex-end;
+        align-items: flex-end;
+        flex-direction: column;
+        padding: 20px;
+        box-sizing: border-box;
+        z-index: 9999;
+        transition: all 0.2s cubic-bezier(0, 0, 0.08108108108108109, 0.95625);
+      `;
+      document.body.appendChild(container);
+      popupContainer = container;
+    }
+    const popup = document.createElement('div');
+    popup.style.cssText = `
+      margin-top: 10px;
+      padding: 10px 20px;
+      background-color: #333;
+      color: white;
+      border-radius: 5px;
+      box-shadow: 0 2px 10px rgba(0,0,0,0.2);
+      z-index: 9999;
+      font-family: Arial, sans-serif;
+    `;
+
+
+
+    popup.style.backgroundColor = backgroundColor;
+    popup.innerHTML = `${emoji} ${level}: ${message}`;
+    popup.id = randId;
+    popupContainer.appendChild(popup);
+    const event = popup.addEventListener('click', () => {
+      if (!popup) {
+        removeEventListener('click', event);
+        return
+      }
+      popup.style.transition = 'transform 0.1s cubic-bezier(0, 0, 0.08108108108108109, 0.95625)';
+      popup.style.transform = 'translateX(105%) rotate(15deg)';
+      setTimeout(() => popupContainer.removeChild(popup), 200);
+      removeEventListener('click', event);
+    })
+    setTimeout(() => {
+      let popup = document.getElementById(randId);
+      if (!popup) return
+      popup.style.transition = 'transform 0.1s cubic-bezier(0, 0, 0.08108108108108109, 0.95625)';
+      popup.style.transform = 'translatey(-205%)';
+      setTimeout(() => popupContainer.removeChild(popup), 200);
+      removeEventListener('click', event);
+    }, 3000);
   }
 
-  l(...args) {
-    this.formatLog('INFO', ...args);
+  showPopupLog(level, message) {
+    this.formatLog(level, message);
+    this.createPopup(level, message);
   }
-  w(...args) {
-    this.formatLog('WARN', ...args);
+  l(...args) {this.formatLog('INFO', ...args);}
+  w(...args) {this.formatLog('WARN', ...args);}
+  e(...args) {this.formatLog('ERROR', ...args);}
+  d(...args) {this.formatLog('DEBUG', ...args);}
+  s(...args) {this.formatLog('SUCCESS', ...args);}
+  i(...args) {this.formatLog('INFO', ...args);}
+  c(...args) {console.log(...args);}
+  get show() {
+    if (isNode) return console;
+    return {
+      l: (...args) => this.showPopupLog('INFO', ...args),
+      w: (...args) => this.showPopupLog('WARN', ...args),
+      e: (...args) => this.showPopupLog('ERROR', ...args),
+      d: (...args) => this.showPopupLog('DEBUG', ...args),
+      s: (...args) => this.showPopupLog('SUCCESS', ...args),
+      i: (...args) => this.showPopupLog('INFO', ...args),
+    }
   }
-  e(...args) {
-    this.formatLog('ERROR', ...args);
-  }
-  d(...args) {
-    this.formatLog('DEBUG', ...args);
-  }
-  s(...args) {
-    this.formatLog('SUCCESS', ...args);
-  }
-  i(...args) {
-    this.formatLog('INFO', ...args);
-  }
-  c(...args) {
-    console.log(...args);
-  }
+    
+}
+return Log;
 }
 
-module.exports = Log;
+if (typeof require !== 'undefined') {
+  module.exports = isolateMiLog();
+}
+if (typeof window !== 'undefined') {
+  window.miLog = isolateMiLog();
+}
