ff5a60219b3e94cda11ff569f1f3707a638bfa4f
micuit-cuit <micuit-cuit@paire.space>
2024-10-27 10:06:39 +0100

    Refactor color handling in log.js

--- log.js
+++ log.js
@@ -2,6 +2,8 @@
 //----creat by micuit-cuit----
 //----------------------------
 //dectecte l'environnement (nodejs ou navigateur)
+//reset le theme de couleur 
+
 function isolateMiLog(){
 let supoort256Color = true;
 const isNode = typeof process !== 'undefined' && process.versions != null && process.versions.node != null;
@@ -9,6 +11,21 @@ if (isNode) {
   supoort256Color = process.env.TERM.includes('256');
 }
 //dectecte si le terminal supporte les couleurs rgb ou ascii
+const colorName = {"black": "#000000","silver": "#c0c0c0","gray": "#808080","white": "#ffffff","maroon": "#800000","red": "#ff0000","purple": "#800080","fuchsia": "#ff00ff","green": "#008000","lime": "#00ff00","olive": "#808000","yellow": "#ffff00","navy": "#000080","blue": "#0000ff","teal": "#008080","aqua": "#00ffff","aliceblue": "#f0f8ff","antiquewhite": "#faebd7","aquamarine": "#7fffd4","azure": "#f0ffff","beige": "#f5f5dc","bisque": "#ffe4c4","blanchedalmond": "#ffebcd","blueviolet": "#8a2be2","brown": "#a52a2a","burlywood": "#deb887","cadetblue": "#5f9ea0","chartreuse": "#7fff00","chocolate": "#d2691e","coral": "#ff7f50","cornflowerblue": "#6495ed","cornsilk": "#fff8dc","crimson": "#dc143c","cyan": "#00ffff","darkblue": "#00008b","darkcyan": "#008b8b","darkgoldenrod": "#b8860b","darkgray": "#a9a9a9","darkgreen": "#006400","darkkhaki": "#bdb76b","darkmagenta": "#8b008b","darkolivegreen": "#556b2f","darkorange": "#ff8c00","darkorchid": "#9932cc","darkred": "#8b0000","darksalmon": "#e9967a","darkseagreen": "#8fbc8f","darkslateblue": "#483d8b","darkslategray": "#2f4f4f","darkturquoise": "#00ced1","darkviolet": "#9400d3","deeppink": "#ff1493","deepskyblue": "#00bfff","dimgray": "#696969","dodgerblue": "#1e90ff","firebrick": "#b22222","floralwhite": "#fffaf0","forestgreen": "#228b22","gainsboro": "#dcdcdc","ghostwhite": "#f8f8ff","gold": "#ffd700","goldenrod": "#daa520","greenyellow": "#adff2f","honeydew": "#f0fff0","hotpink": "#ff69b4","indianred": "#cd5c5c","indigo": "#4b0082","ivory": "#fffff0","khaki": "#f0e68c","lavender": "#e6e6fa","lavenderblush": "#fff0f5","lawngreen": "#7cfc00","lemonchiffon": "#fffacd","lightblue": "#add8e6","lightcoral": "#f08080","lightcyan": "#e0ffff","lightgoldenrodyellow": "#fafad2","lightgray": "#d3d3d3","lightgreen": "#90ee90","lightpink": "#ffb6c1","lightsalmon": "#ffa07a","lightseagreen": "#20b2aa","lightskyblue": "#87cefa","lightslategray": "#778899","lightsteelblue": "#b0c4de","lightyellow": "#ffffe0","limegreen": "#32cd32","linen": "#faf0e6","mediumaquamarine": "#66cdaa","mediumblue": "#0000cd","mediumorchid": "#ba55d3","mediumpurple": "#9370db","mediumseagreen": "#3cb371","mediumslateblue": "#7b68ee","mediumspringgreen": "#00fa9a","mediumturquoise": "#48d1cc","mediumvioletred": "#c71585","midnightblue": "#191970","mintcream": "#f5fffa","mistyrose": "#ffe4e1","moccasin": "#ffe4b5","navajowhite": "#ffdead","oldlace": "#fdf5e6","olivedrab": "#6b8e23","orange": "#ffa500","orangered": "#ff4500","orchid": "#da70d6","palegoldenrod": "#eee8aa","palegreen": "#98fb98","paleturquoise": "#afeeee","palevioletred": "#db7093","papayawhip": "#ffefd5","peachpuff": "#ffdab9","peru": "#cd853f","pink": "#ffc0cb","plum": "#dda0dd","powderblue": "#b0e0e6","rebeccapurple": "#663399","rosybrown": "#bc8f8f","royalblue": "#4169e1","saddlebrown": "#8b4513","salmon": "#fa8072","sandybrown": "#f4a460","seagreen": "#2e8b57","seashell": "#fff5ee","sienna": "#a0522d","skyblue": "#87ceeb","slateblue": "#6a5acd","slategray": "#708090","snow": "#fffafa","springgreen": "#00ff7f","steelblue": "#4682b4","tan": "#d2b48c","thistle": "#d8bfd8","tomato": "#ff6347","turquoise": "#40e0d0","violet": "#ee82ee","wheat": "#f5deb3","whitesmoke": "#f5f5f5","yellowgreen": "#9acd32","brightBlack": "#545454","brightRed": "#FF5555","brightGreen": "#55FF55","brightYellow": "#FFFF55","brightBlue": "#5555FF","brightMagenta": "#FF55FF","brightCyan": "#55FFFF","brightWhite": "#FFFFFF"}
+  
+function colorToHex(color) {
+  if (colorName[color]) {
+    return colorName[color];
+  }else if (color[0] === '#') {
+    return color;
+  }else if (color.startsWith("rgb")) {
+    let [r, g, b] = color.match(/\d+/g);
+    return `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`;
+  }else{
+    console.error(`Color ${color} not use, support only hex, rgb, rgba or css color name`);
+    return "#ff0000";
+  }
+}
 
 const darkColor = [
   "blue"
@@ -30,31 +47,42 @@ class Log {
       return { fg: 'black', bg: color };
     }
     if (supoort256Color) {
-      r = Math.max(0, Math.min(255, r));
-      g = Math.max(0, Math.min(255, g));
-      b = Math.max(0, Math.min(255, b));
+      if (color === 'reset') {
+        return '\x1b[0m';
+      }
+      // Convertit les couleurs hexadécimales en RGB
+      color = colorToHex(color);
+      if (color[0] === '#') {
+        color = color.slice(1);
+      }
+      if (color.length === 3) {
+        color = color.split('').map((x) => x + x).join('');
+      }
+      let r = parseInt(color.slice(0, 2), 16);
+      let g = parseInt(color.slice(2, 4), 16);
+      let b = parseInt(color.slice(4, 6), 16);
 
       // Génère la séquence d'échappement ANSI pour les couleurs RGB
-      return { fg: `\x1b[38;2;${r};${g};${b}m`, bg: `\x1b[40m` };
+      return `\x1b[38;2;${r};${g};${b}m`;
     }
     const colors = {
-      reset: '\x1b[0m\x1b[40m',
-      black: { fg: '\x1b[30m', bg: '\x1b[40m' },
-      red: { fg: '\x1b[31m', bg: '\x1b[41m' },
-      green: { fg: '\x1b[32m', bg: '\x1b[42m' },
-      yellow: { fg: '\x1b[33m', bg: '\x1b[43m' },
-      blue: { fg: '\x1b[34m', bg: '\x1b[44m' },
-      magenta: { fg: '\x1b[35m', bg: '\x1b[45m' },
-      cyan: { fg: '\x1b[36m', bg: '\x1b[46m' },
-      white: { fg: '\x1b[37m', bg: '\x1b[47m' },
-      brightBlack: { fg: '\x1b[90m', bg: '\x1b[100m' },
-      brightRed: { fg: '\x1b[91m', bg: '\x1b[101m' },
-      brightGreen: { fg: '\x1b[92m', bg: '\x1b[102m' },
-      brightYellow: { fg: '\x1b[93m', bg: '\x1b[103m' },
-      brightBlue: { fg: '\x1b[94m', bg: '\x1b[104m' },
-      brightMagenta: { fg: '\x1b[95m', bg: '\x1b[105m' },
-      brightCyan: { fg: '\x1b[96m', bg: '\x1b[106m' },
-      brightWhite: { fg: '\x1b[97m', bg: '\x1b[107m' },
+      reset: '\x1b[0m',
+      black: '\x1b[30m',
+      red: '\x1b[31m',
+      green: '\x1b[32m',
+      yellow: '\x1b[33m',
+      blue: '\x1b[34m',
+      magenta: '\x1b[35m',
+      cyan: '\x1b[36m' ,
+      white: '\x1b[37m',
+      brightBlack: '\x1b[90m',
+      brightRed: '\x1b[91m',
+      brightGreen: '\x1b[92m',
+      brightYellow: '\x1b[93m',
+      brightBlue: '\x1b[94m',
+      brightMagenta: '\x1b[95m',
+      brightCyan: '\x1b[96m',
+      brightWhite: '\x1b[97m',
     };
     if (colors[color]) {
       return colors[color];
@@ -71,23 +99,23 @@ class Log {
     switch (level) {
       case 'INFO':
         emoji = 'ℹ️';
-        color = this.colorize('brightBlue').fg+this.colorize('black').bg+":dodgerblue"
+        color = this.colorize('brightBlue')+":dodgerblue"
         break;
       case 'WARN':
         emoji = '⚠️';
-        color = this.colorize('brightYellow').fg+this.colorize('black').bg+":orange";
+        color = this.colorize('brightYellow')+":orange";
         break;
       case 'ERROR':
         emoji = '🚫';
-        color = this.colorize('brightRed').fg+this.colorize('black').bg+":red";
+        color = this.colorize('brightRed')+":red";
         break;
       case 'DEBUG':
         emoji = '🐛';
-        color = this.colorize('brightMagenta').fg+this.colorize('black').bg+":purple";
+        color = this.colorize('brightMagenta')+":purple";
         break;
       default:
         emoji = '✅';
-        color = this.colorize('brightGreen').fg+this.colorize('black').bg+":green";
+        color = this.colorize('brightGreen')+":green";
     }
     if (!isNode) {
       color = color.split(':')[1];
@@ -99,10 +127,10 @@ class Log {
     let message = '';
     let prefixes = [
       { style: 'circle', color: color, text: `${emoji} ${level}` },
-      { style: 'square', color: isNode?this.colorize('yellow').fg+this.colorize('black').bg:"yellow", text: `${time}` },
+      { style: 'square', color: isNode?this.colorize('yellow'):"yellow", text: `${time}` },
       { style: 'square', color: color, text: `${file}:${line}` },
       ...this.prefixe.map((item) => {
-        return { style:"circle", color: isNode?this.colorize(item.color).fg+this.colorize('black').bg:item.color, text: item.text };
+        return { style:"circle", color: isNode?this.colorize(item.color):item.color, text: item.text };
       })
     ];
     if (isNode){ 
@@ -129,7 +157,7 @@ class Log {
       console.log(style.join(' '), ...args2, this.colorize('reset'));
     }else{
       for (let i = 0; i < prefixes.length; i++) {
-        if (!prefixes[i].style == "square") {
+        if (!prefixes[i].style == "circle<") {
           style.push(`background: ${prefixes[i].color}; color: ${darkColor.includes(prefixes[i].color) ? "white" : "black"}; font-weight: bold; border-radius: 5px;`);
           style.push("["+prefixes[i].text+"]");
         }else{
